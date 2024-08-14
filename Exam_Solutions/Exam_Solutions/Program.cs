27cb07b04c605b4f63c8e77d201d0f48f94b3a04
safran3k <futo.ricsi@gmail.com>
2024-08-14 12:35:27 +0200

    Upload project

---
+++ Exam_Solutions/Exam_Solutions/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_Solutions
+{
+    class Program
+    {
+        static double Elso_feladat(int n)
+        {
+            if (n == 0)
+            {
+                return 5;
+            }
+            return (Math.Pow(Elso_feladat(n - 1), 2) + 2 * Elso_feladat(n - 1)) / 10;
+        }
+
+        public static int Lnko(int a, int b)
+        {
+            if (a < b)
+            {
+                a = a + b;
+                b = a - b;
+                a = a - b;
+            }
+            else if (b == 0)
+            {
+                return a;
+            }
+
+            return Lnko(b, a - b);
+        }
+
+        static int Masodik_feladat(int a, int b)
+        {
+            return (a * b) / Lnko(a, b);
+        }
+
+        static int Harmadik_feladat(int c1, int c2, int n)
+        {
+            if (n == 0)
+            {
+                return c1;
+            }
+
+            if (n == 1)
+            {
+                return c2;
+            }
+
+            return Harmadik_feladat(c1, c2, n - 2) + Harmadik_feladat(c1, c2, n - 1);
+        }
+
+        static int Vsetkojedno(int a, int d)
+        {
+            if (a == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                d = Vsetkojedno(a / 2, d);
+                return d + (a % 2);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. feladat");
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.Write(Elso_feladat(i) + " ");
+            }
+
+
+            Console.WriteLine("\n\n------------------------------\n");
+            Console.WriteLine("2. feladat");
+            Console.Write("Adja meg az A számot: ");
+            int input_a = int.Parse(Console.ReadLine());
+            Console.Write("Adja meg az B számot: ");
+            int input_b = int.Parse(Console.ReadLine());
+            Console.WriteLine($"A legkissebb közös többszörö ({input_a};{input_b})-nek: {Masodik_feladat(input_a, input_b)}");
+
+
+            Console.WriteLine("\n\n------------------------------\n");
+            Console.WriteLine("3. feladat");
+            Console.Write("Adja meg a c1-et: ");
+            int input_c1 = int.Parse(Console.ReadLine());
+            Console.Write("Adja meg a c2-et: ");
+            int input_c2 = int.Parse(Console.ReadLine());
+            Console.Write("Hányadik tagra (n) kíváncsi: ");
+            int input_n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            for (int i = 0; i < input_n; i++)
+            {
+                Console.Write(Harmadik_feladat(input_c1, input_c2, i) + " ");
+            }
+
+
+            Console.WriteLine("\n\n------------------------------\n");
+            Console.WriteLine("4. feladat");
+            int d = 0;
+            Console.WriteLine($"Eredmény: {Vsetkojedno(35, d)}");
+
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# Data_Structures_and_Algorithms_II - Exam_Solutions
- 
+# Data Structures and Algorithms II - Exam Solutions
+
+This project contains solutions to a series of algorithmic problems given as part of the **Data Structures and Algorithms II** course. The problems are focused on implementing recursive algorithms and understanding basic algorithmic concepts.
+
+## Problems and Solutions
+
+1. **Recursive Sequence Calculation**
+   - Implemented a recursive function to calculate the elements of a sequence defined by the following recurrence relation:
+     \[
+     a_i = egin{cases}
+     5 & 	ext{ha } i = 0 \
+     rac{1}{10} \cdot \left(2 + 2 \cdot a_{i-1}
+ight) & 	ext{egyébként}
+     \end{cases}
+     \]
+   - The function calculates and prints the first 11 elements of the sequence.
+
+2. **Least Common Multiple (LCM) Calculation**
+   - Implemented a recursive function to calculate the least common multiple (LCM) of two numbers using the greatest common divisor (GCD).
+
+3. **Generalized Fibonacci Sequence**
+   - Created a recursive algorithm to generate a generalized Fibonacci sequence based on two input numbers (`c1`, `c2`). The function generates and prints the first `n` elements of the sequence.
+
+4. **Recursive Algorithm Analysis**
+   - Analyzed a given recursive algorithm named `Vsetkojedno` and implemented it in C#. The algorithm performs bitwise operations to calculate a specific value based on the binary representation of an input number.
+
+## How to Run
+
+1. **Clone the repository** to your local machine.
+2. Open the project in a C# compatible IDE (e.g., Visual Studio).
+3. **Run the project** to execute the solutions. You will be prompted to input values where necessary.
+4. The outputs will be printed directly in the console.
+
+## License
+
+This project is for educational purposes as part of the Data Structures and Algorithms II course.
